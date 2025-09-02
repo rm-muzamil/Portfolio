b5f7767c49f5052193c79431ad89d7d67820ccb8
RM Muzammil <ramuzamil44@gmail.com>
2025-09-02 22:43:09 +0500

    bug fixed

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,5 @@
 // components/Profile.tsx
-
+import Image from "next/image";
 import "../../styles/Profile.css";
 
 export default function Profile() {
@@ -8,7 +8,13 @@ export default function Profile() {
       <div className="background-lines" />
       <div className="profile-container">
         <div className="profile-bg">
-          <img src="/rm.png" alt="Profile" className="profile-image" />
+          <Image
+            src="/rm.png"
+            width={500}
+            height={500}
+            alt="Profile"
+            className="profile-image"
+          />
         </div>
       </div>
     </>
